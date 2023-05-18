7ee5a22752a82efe468f0f970f5317c137e0a51a
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-18 20:11:25 -0300

    change color of title

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         "fern-50": "#f5f9f4",
+        "fern-200": "#e7f3e5",
         "fern-400": "#6daf64",
         "fern-800": "#2f502b",
         "fern-900": "#112310"
