d51e8d61013945fdc593eb3c023f2072044a8763
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-18 19:10:37 -0300

    add links to social media and cv download

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors: {
         "fern-50": "#f5f9f4",
         "fern-400": "#6daf64",
-        "fern-800": "#2f502b"
+        "fern-800": "#2f502b",
+        "fern-900": "#112310"
       }
     },
   },
