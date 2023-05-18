d9adc3d8d473fba4af471bc0c4f6a9d14f0ff84e
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-17 22:25:26 -0300

    create card with personal info]

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "sandy-brown-50": "#fef8ee",
+        "sandy-brown-100": "#fdf0d7",
+        "sandy-brown-200": "#faddae",
+        "sandy-brown-300": "#f6c47b",
+        "sandy-brown-400": "#f3ab59",
+        "sandy-brown-500": "#ee8621",
+        "sandy-brown-600": "#df6b17",
+        "sandy-brown-700": "#b95215",
+        "sandy-brown-800": "#934119",
+        "sandy-brown-900": "#773817",
+        "sandy-brown-950": "#401a0a"
+      }
+    },
+  },
+  plugins: [],
+}
+
