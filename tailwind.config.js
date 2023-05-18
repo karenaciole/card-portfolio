307f98e5a68f97b10f7d59001f1371f3b2d117a5
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-18 18:11:00 -0300

    change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "sandy-brown-50": "#fef8ee",
-        "sandy-brown-100": "#fdf0d7",
-        "sandy-brown-200": "#faddae",
-        "sandy-brown-300": "#f6c47b",
-        "sandy-brown-400": "#f3ab59",
-        "sandy-brown-500": "#ee8621",
-        "sandy-brown-600": "#df6b17",
-        "sandy-brown-700": "#b95215",
-        "sandy-brown-800": "#934119",
-        "sandy-brown-900": "#773817",
-        "sandy-brown-950": "#401a0a"
+        "fern-50": "#f5f9f4",
+        "fern-400": "#6daf64",
+        "fern-800": "#2f502b"
       }
     },
   },
