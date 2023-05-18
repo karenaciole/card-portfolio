d96e021a8d6324b7319ae9ddeb7095de21f8a74f
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-18 20:48:55 -0300

    add terminal layout in text

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         "fern-50": "#f5f9f4",
         "fern-200": "#e7f3e5",
+        "fern-300": "#a8d3a2",
         "fern-400": "#6daf64",
         "fern-800": "#2f502b",
         "fern-900": "#112310"
